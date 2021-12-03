bb5f079a83ffdc826309a1f9836eef500e0b09e8
Sarthak Srivastava <95467726+s-sarthak@users.noreply.github.com>
2021-12-03 20:10:37 +0530

    Update settings.py

--- teacherrecord/teacherrecord/settings.py
+++ teacherrecord/teacherrecord/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'r_1!$=er)p43!*mz-oy2st7tr$mvlq#zn!$u&x80wd4q#-$eq2'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
